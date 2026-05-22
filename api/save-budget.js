c3258464d0e3ca559dccdfdea28b457373be8458
Slimpe <slimpe@example.com>
2026-05-22 09:25:09 -0300

    chore: hardcode database url for vercel

--- api/save-budget.js
+++ api/save-budget.js
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_DtfA7VXHw8ym@ep-winter-cloud-apstwhit-pooler.c-7.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require",
   ssl: {
     rejectUnauthorized: false
   }
